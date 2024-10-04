1e4ab7651d9fabdd387e65ee093c819beec8d4b0
natanaelcloud <natanpaivacosta@gmail.com>
2024-10-04 18:27:07 -0300

    Adicionar Function de nome getRandomColor

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
-const colors = ['Vermelho', 'Azul', 'Amarelo', 'Roxo', 'Verde']
+const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green']
 const btn = document.getElementById('btn')
 
 btn.addEventListener('click', function(){
-    
+    document.body.style.background = colors[]
+    const ramdomColor = getRandomColor()
 })
+
+function getRandomColor(){
+    return Map.floor( Math.random() * colors.length)
+}
