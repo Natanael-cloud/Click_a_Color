e5e656a30759e4e1ca4a3606dc988fd1beb7ce2a
natanaelcloud <natanpaivacosta@gmail.com>
2024-10-04 18:55:01 -0300

    Comentar codigo

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+// Declara um array de cores, que contém cinco valores de cor
+const colors = ['green', 'yellow', 'purple', 'blue', 'red']
 
-const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green']
+// Seleciona o botão no HTML com o id 'btn' e o armazena na constante 'btn'
 const btn = document.getElementById('btn')
+
+// Seleciona o elemento no HTML que tem a classe 'color' e o armazena na constante 'color'
 const color = document.querySelector('.color')
 
-btn.addEventListener('click', function(){
-    const ramdomColor = getRandomColor()
-    document.body.style.background = colors[ramdomColor]
+// Adiciona um ouvinte de evento (event listener) que aguarda o clique no botão 'btn'
+btn.addEventListener('click', function() {
+    // Chama a função getRandomColor para obter um índice aleatório do array 'colors'
+    const randomColor = getRandomColor()
+
+    // Define a cor de fundo do corpo do documento para uma cor do array, baseada no índice aleatório
+    document.body.style.background = colors[randomColor]
+
+    // Atualiza o texto do elemento '.color' para exibir a cor escolhida
+    color.textContent = colors[randomColor]
 })
 
-function getRandomColor(){
-    return Map.floor( Math.random() * colors.length)
+// Função que gera um número aleatório dentro do limite do tamanho do array 'colors'
+// Retorna um número entre 0 e o tamanho do array 'colors'
+function getRandomColor() {
+    return Math.floor(Math.random() * colors.length)
 }
